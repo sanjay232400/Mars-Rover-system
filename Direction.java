ed93dec89068d45fe41978aceae7a9645d6afe0b
sanjay232400 <ksanjay06129@gmail.com>
2024-07-28 22:25:38 +0530

    Create Direction.java

---
+++ Direction.java
@@ -0,0 +1,11 @@
+public enum Direction {
+    N, E, S, W;
+
+    public Direction left() {
+        return values()[(ordinal() + 3) % 4];
+    }
+
+    public Direction right() {
+        return values()[(ordinal() + 1) % 4];
+    }
+}
